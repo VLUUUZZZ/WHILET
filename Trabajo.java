874ee5640f0b7539a1a34f11b5bc22c9777cc669
uzzielvictoor <xiroda6729@marchub.com>
2025-05-22 08:53:36 -0600

    INGRESA DOS NUMEROS Y TE LOS DIVIDE Y NO ACEPTA CEROS NI NUMEROS NEGATIVOS

---
+++ Trabajo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Trabajo {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int obtenerNumeros(String mensaje) {
+        int numeroTemporal = 0;
+        do {
+            System.out.println(mensaje);
+            numeroTemporal = scanner.nextInt();
+            if (numeroTemporal <= 0) {
+                System.out.println("Por favor, ingresa un número positivo mayor a cero.");
+            }
+        } while (numeroTemporal <= 0);
+        return numeroTemporal;
+    }
+
+    public static void main(String[] args) {
+        int numero = obtenerNumeros("Ingresa un número:");
+        int numero2 = obtenerNumeros("Ingresa otro número:");
+        int resultado = numero + numero2;
+        System.out.println("El resultado es: " + resultado);
+    }
+}
